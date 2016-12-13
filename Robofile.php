a1a014d2d2db1ec17716b6778de03848c6871bd9
Dieter Blomme <dieterblomme@gmail.com>
2016-12-13 13:43:15 +0100

    Add basis for tests and some failing tests

---
+++ Robofile.php
@@ -0,0 +1,18 @@
+<?php
+class RoboFile extends \Robo\Tasks
+{
+  use DigipolisGent\Robo\Task\DrupalConsole\loadTasks;
+
+  public function test()
+  {
+    $this->stopOnFail(true);
+    $this->taskPHPUnit()
+      ->option('disallow-test-output')
+      ->option('report-useless-tests')
+      ->option('strict-coverage')
+      ->option('-v')
+      ->option('-d error_reporting=-1')
+      ->arg('tests')
+      ->run();
+  }
+}
---
+++ tests/DrupalConsoleStackTest.php
@@ -0,0 +1,65 @@
+<?php
+
+class DrushStackTest extends \PHPUnit_Framework_TestCase
+{
+  use DigipolisGent\Robo\Task\Drush\loadTasks;
+
+  public function testYesIsAssumed()
+  {
+    $command = $this->taskDrupalConsoleStack()
+      ->exec('command')
+      ->getCommand();
+    $this->assertEquals('drupal command --yes', $command);
+  }
+
+  public function testAbsenceofYes()
+  {
+    $command = $this->taskDrupalConsoleStack()
+      ->exec('command', false)
+      ->getCommand();
+    $this->assertEquals('drupal command', $command);
+  }
+
+  public function testOptionsArePrependedBeforeEachCommand()
+  {
+    $command = $this->taskDrupalConsoleStack()
+      ->drupalRootDirectory('/var/www/html/app')
+      ->exec('command-1')
+      ->exec('command-2')
+      ->getCommand();
+    $this->assertEquals(2, preg_match_all('#-r /var/www/html/app#', $command));
+  }
+
+  public function testSiteInstallCommand()
+  {
+    $command = $this->taskDrupalConsoleStack()
+      ->siteName('Site Name')
+      ->siteMail('site-mail@example.com')
+      ->locale('de')
+      ->accountMail('mail@example.com')
+      ->accountName('admin')
+      ->accountPass('pw')
+      ->dbPrefix('drupal_')
+      ->dbType('sqlite')
+      ->dbFile('sites/default/.ht.sqlite')
+      ->disableUpdateStatusModule()
+      ->siteInstall('minimal')
+      ->getCommand();
+    $expected = 'drupal site:install minimal --yes --site-name=' . escapeshellarg('Site Name')
+      . ' --site-mail=site-mail@example.com'
+      . ' --langcode=de --account-mail=mail@example.com --account-name=' . escapeshellarg('admin')
+      . ' --account-pass=pw'
+      . ' --db-prefix=drupal_ --db-type=mysql --db-type=mysql' . escapeshellarg('sites/default/.ht.sqlite');
+    $this->assertEquals($expected, $command);
+  }
+
+  public function testDrupalConsoleStatus()
+  {
+    $result = $this->taskDrupalConsoleStack(__DIR__ . '/../vendor/bin/drupal')
+      ->printed(false)
+      ->status()
+      ->run();
+    $this->assertTrue($result->wasSuccessful(), 'Exit code was: ' . $result->getExitCode());
+  }
+
+}
