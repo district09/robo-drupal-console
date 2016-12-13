0e4725dd2219f07d9fdaf41ee385782267154b71
Dieter Blomme <dieterblomme@gmail.com>
2016-12-13 17:12:53 +0100

    Fix so tests will run, use containerawareinterface and taskaccessor trait

--- tests/DrupalConsoleStackTest.php
+++ tests/DrupalConsoleStackTest.php
@@ -1,8 +1,27 @@
 <?php
 
-class DrupalConsoleStackTest extends \PHPUnit_Framework_TestCase {
+use League\Container\ContainerAwareInterface;
+use League\Container\ContainerAwareTrait;
+use Symfony\Component\Console\Output\NullOutput;
+use Robo\TaskAccessor;
+use Robo\Robo;
+
+class DrupalConsoleStackTest extends \PHPUnit_Framework_TestCase implements ContainerAwareInterface {
 
   use DigipolisGent\Robo\Task\DrupalConsole\loadTasks;
+  use TaskAccessor;
+  use ContainerAwareTrait;
+
+  // Set up the Robo container so that we can create tasks in our tests.
+  public function setup() {
+    $container = Robo::createDefaultContainer(null, new NullOutput());
+    $this->setContainer($container);
+  }
+  // Scaffold the collection builder
+  public function collectionBuilder() {
+    $emptyRobofile = new \Robo\Tasks;
+    return $this->getContainer()->get('collectionBuilder', [$emptyRobofile]);
+  }
 
   public function testYesIsAssumed() {
     $command = $this->taskDrupalConsoleStack()
