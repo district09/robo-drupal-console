87cc3c7b883efc5320b20f5f0684c24a76fede02
Dieter Blomme <dieterblomme@gmail.com>
2016-12-14 02:36:19 +0100

    Fix coding style in Robofile.php

--- RoboFile.php
+++ RoboFile.php
@@ -1,19 +1,21 @@
 <?php
 
-class RoboFile extends \Robo\Tasks {
+class RoboFile extends \Robo\Tasks
+{
 
-  use DigipolisGent\Robo\Task\DrupalConsole\loadTasks;
+    use DigipolisGent\Robo\Task\DrupalConsole\loadTasks;
 
-  public function test() {
-    $this->stopOnFail(true);
-    $this->taskPHPUnit()
-      ->option('disallow-test-output')
-      ->option('report-useless-tests')
-      ->option('strict-coverage')
-      ->option('-v')
-      ->option('-d error_reporting=-1')
-      ->arg('tests')
-      ->run();
-  }
+    public function test()
+    {
+        $this->stopOnFail(true);
+        $this->taskPHPUnit()
+          ->option('disallow-test-output')
+          ->option('report-useless-tests')
+          ->option('strict-coverage')
+          ->option('-v')
+          ->option('-d error_reporting=-1')
+          ->arg('tests')
+          ->run();
+    }
 
 }
