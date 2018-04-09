234a059ac76705575ac6b08499d8d68f343bf755
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2018-04-09 15:17:17 +0200

    Fix tests.

--- tests/DrupalConsoleStackTest.php
+++ tests/DrupalConsoleStackTest.php
@@ -285,7 +285,7 @@ class DrupalConsoleStackTest extends \PHPUnit_Framework_TestCase implements Cont
             ->listCommands()
             ->run();
         $this->assertEquals(
-            '[ERROR] DrupalConsole must be executed within a Drupal Site.',
+            'DrupalConsole must be executed within a Drupal Site.',
             trim($result->getMessage())
         );
         $this->assertFalse(
